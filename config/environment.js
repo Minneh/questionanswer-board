7e3d00846969ecf2eb95ce2e795d7e5b812894d6
Minneh Mugo <minneh.mugo@gmail.com>
2017-03-30 12:55:42 +0300

    Include add and delete functionality for questions included buttons to add and delete questions

--- config/environment.js
+++ config/environment.js
@@ -11,12 +11,6 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
-      firebase: {
-        apiKey: "AIzaSyD8wmRMIUuRtyHz-jkpfMJAAAMxwNDlg0k",
-      authDomain: "qnaboard-183fe.firebaseapp.com",
-      databaseURL: "https://qnaboard-183fe.firebaseio.com",
-      storageBucket: "qnaboard-183fe.appspot.com",
-    },
 
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
@@ -24,6 +18,12 @@ module.exports = function(environment) {
       }
     },
 
+      firebase: {
+      apiKey: 'AIzaSyD8wmRMIUuRtyHz-jkpfMJAAAMxwNDlg0k',
+      authDomain: 'qnaboard-183fe.firebaseapp.com',
+      databaseURL: 'https://qnaboard-183fe.firebaseio.com',
+      storageBucket: 'qnaboard-183fe.appspot.com'
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
