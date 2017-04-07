09b20e9fbb9ec354a181ef2faa21b4201eb272ae
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-07 12:46:39 +0300

    add computed property
    combined username and answer to form computed property

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  fullAnswer: Ember.computed('answer.content', 'answer.author', function() {
+    return this.get('answer.content') + ' - by ' + this.get('answer.author');
+  }),
   actions: {
     delete(answer){
       if(confirm("Are you sure you want to delete this answer?")){
