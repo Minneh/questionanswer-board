898790bbba810b81766763eaa028989f68608de3
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-10 21:31:56 +0300

    add commenting to question-popularity and favorite-questions

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export function questionPopularity(params) {
+  // set variable question to first element of params array
   var question = params[0];
-
+// if question has more than five answers, include a fire icon beside it
   if(question.get('answers').get('length') >= 5) {
    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
   }
 }
-
+// make questionPopularity helper available to other files
 export default Ember.Helper.helper(questionPopularity);
--- app/services/favorite-questions.js
+++ app/services/favorite-questions.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  // declare blank array questions[]
   questions: [],
-
+// add passed question to array
   add(question){
     this.get('questions').pushObject(question);
   },
+  // remove passed question from array 
   delete(question){
     this.get('questions').removeObject(question);
   }
